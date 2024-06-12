9f55890550568a3035ca6145b54746deaf968aa6
dangoodd <dangoodds@gmail.com>
2024-06-12 17:36:48 +0300

    better exstensibility

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 Useful justfile templates repository.
 
 Contains functions for fast creating of justfiles.
+Basically wrapper around cp command)
 
 ## Confuguration
 For change directory that contains language/justfile directories
 to custom set JUST_TEMPLATES_DIR
+
+## Usage
+For all templates in this repo:
+```zsh
+just-language /destination/path
+```
+For any other templates (function will search justfile in source path):
+```zsh
+just-template /source/path /destination/path
+```
--- just-templates.zsh
+++ just-templates.zsh
@@ -1,20 +1,27 @@
 # templates for justfiles
 [[ -z $JUST_TEMPLATES_DIR ]] && JUST_TEMPLATES_DIR=${0:A:h}
 
-# latex
-function just-latex() {
-    src="$JUST_TEMPLATES_DIR"/latex/justfile
-    dst="$PWD"
+function just-template() {
+    src="$1"/justfile
+    dst="$2"
+    [[ -z $dst ]] && dst=$PWD
 
-    if ! [[ -d "@" ]]; then
-        echo "$@: incorrect directory"
-        exit 0
+    if ! [[ -d "$dst" ]]; then
+        echo "$dst: incorrect dst directory"
+        return 0
     fi 
 
     if ! [[ -f "$src" ]]; then 
-        echo "incorrect JUST_TEMPLATES_DIR value"
-        exit 0 
+        echo "$src: incorrect src value"
+        return 0 
     fi
 
     cp "$src" "$dst"
+}
+
+# latex
+function just-latex() {
+    src="$JUST_TEMPLATES_DIR"/latex/
+    dst="$@"
+    just-template "$src" "$dst"
 }
